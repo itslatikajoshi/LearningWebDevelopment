c1c20fb5e3ac7d19e0ff922e5620fe2e1ba70e8b
Latika Joshi <itslatikajoshi@gmail.com>
2023-06-18 01:39:54 +0530

    new exercise

---
+++ exercise1.php
@@ -0,0 +1,23 @@
+<?php
+
+    #constants in php
+
+    define("SCHOOL","Jaycees");
+
+    echo SCHOOL;
+    #variable
+    $Gaurav = "Latika";
+    $name = $Gaurav;
+    #reference variable
+    $newname = $$name;
+    echo $newname;
+    
+    $x = "abc";  
+    $$x = 200;  
+    echo $x."<br/>";  
+    echo $$x."<br/>";  
+    echo $abc;  
+
+    ECHO "ram";
+
+?>
