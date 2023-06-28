f6e0dd5592c0e7375190e40e3aaa8b02e029a7f0
Latika Joshi <itslatikajoshi@gmail.com>
2023-06-28 22:32:21 +0530

    php tut

---
+++ php/01_variable.php
@@ -0,0 +1,13 @@
+<?php
+$name = "Shubham";
+// Variables are case sensitive
+$namE = "Capital wala Shubham";
+$income = 200.8;
+
+echo "This guy is $name and his income is Rs. $income <br>";
+
+echo "$namE is a good boy <br>";
+echo "$name is the real gangsta <br>";
+
+
+?>
