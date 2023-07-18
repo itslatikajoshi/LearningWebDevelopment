c775fe124d8d6e7fdc400f4b45849faf486f1b31
Latika Joshi <itslatikajoshi@gmail.com>
2023-07-18 11:24:36 +0530

    functions

--- php/12_functions.php
+++ php/12_functions.php
@@ -2,18 +2,20 @@
 
 echo "Functions in php";
 
-function processMarks ($marksarr)
+function processMarks ($marksArr)
 {
     $sum =0;
-    foreach($marksarr as $value)
+    foreach($marksArr as $value)
     {
         $sum += $value;
     }
-    return $value;
+    return $sum;
 }
 
 $rohanDas =[34,98,45,12,98,93];
 $sumMarks = processMarks($rohanDas);
 echo "Total marks scored by Rohan out of 600 is $sumMarks";
-
-?>
+echo"<br>";
+$Latika =[54,78,65,52,98,93];
+$sumMarks = processMarks($Latika);
+echo "Total marks scored by Latika out of 600 is $sumMarks";
