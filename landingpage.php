6a6591fe8b7c64f54465ff0782f88d148f31be74
Latika Joshi <itslatikajoshi@gmail.com>
2023-08-18 14:36:18 +0530

    add-ons

---
+++ landingpage.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Stately World</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      background-color: #f4f4f4;
+    }
+
+    .container {
+      max-width: 1200px;
+      margin: 0 auto;
+      padding: 20px;
+    }
+
+    .header {
+      text-align: center;
+      padding: 50px;
+      background-color: #3498db;
+      color: #fff;
+    }
+
+    .headline {
+      font-size: 28px;
+      margin-bottom: 20px;
+    }
+
+    .subheadline {
+      font-size: 18px;
+      color: #888;
+      margin-bottom: 40px;
+    }
+
+    .cta-button {
+      display: inline-block;
+      padding: 10px 20px;
+      background-color: #e74c3c;
+      color: #fff;
+      font-size: 16px;
+      text-decoration: none;
+      border-radius: 5px;
+    }
+
+    .features {
+      display: flex;
+      justify-content: space-around;
+      margin-top: 40px;
+    }
+
+    .feature {
+      text-align: center;
+      padding: 20px;
+    }
+
+    .feature-icon {
+      font-size: 40px;
+      color: #3498db;
+    }
+
+    .feature-title {
+      font-size: 18px;
+      margin-top: 10px;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="header">
+    <h1 class="headline">Welcome to Stately World : Your Own Technology World</h1>
+    <p class="subheadline">Discover the amazing features that awaits you.</p>
+    <a class="cta-button" href="#">Get Started</a>
+  </div>
+  <div class="container">
+    <div class="features">
+      <div class="row-1">
+        <div class="feature">
+          <div class="feature-icon">&#x1F4B3;</div>
+          <h2 class="feature-title">Website Development:
+</h2>
+          <p>Creating websites from scratch using programming languages like HTML, CSS, and JavaScript. This can include static websites, dynamic websites with databases, and content management system (CMS) integration.
+</p>
+        </div>
+        <div class="feature">
+          <div class="feature-icon">&#x1F4B8;</div>
+          <h2 class="feature-title">Web Application Development:
+</h2>
+          <p>Building interactive and feature-rich web applications that can perform complex tasks. These applications often involve backend development, APIs, and various technologies like React, Angular, or Vue for frontend development.
+</p>
+        </div>
+        <div class="feature">
+          <div class="feature-icon">&#x1F4A1;</div>
+          <h2 class="feature-title">E-Commerce Solutions:
+</h2>
+          <p>Developing online stores and e-commerce platforms, integrating payment gateways, shopping carts, and inventory management systems.
+</p>
+        </div>
+      </div>
+      <div class="row-1">
+        <div class="feature">
+          <div class="feature-icon">&#x1F4B3;</div>
+          <h2 class="feature-title">Custom CMS Development:
+</h2>
+          <p>Building content management systems tailored to the client's specific needs, allowing them to manage their website's content easily.
+</p>
+        </div>
+        <div class="feature">
+          <div class="feature-icon">&#x1F4B8;</div>
+          <h2 class="feature-title">Responsive Design:
+</h2>
+          <p>Creating websites and applications that work well on various devices and screen sizes, ensuring a seamless user experience across desktops, tablets, and smartphones.
+</p>
+        </div>
+        <div class="feature">
+          <div class="feature-icon">&#x1F4A1;</div>
+          <h2 class="feature-title">UI/UX Design:
+</h2>
+          <p>Designing user interfaces and experiences that are visually appealing, easy to navigate, and user-friendly. This may involve wireframing, prototyping, and user testing.
+</p>
+        </div>
+      </div>
+      <div class="row-1">
+        <div class="feature">
+          <div class="feature-icon">&#x1F4B3;</div>
+          <h2 class="feature-title">Frontend Development:
+</h2>
+          <p>Building the user interface and user interactions of websites and web applications using HTML, CSS, and JavaScript. This can include animations, responsive design, and accessibility considerations.
+</p>
+        </div>
+        <div class="feature">
+          <div class="feature-icon">&#x1F4B8;</div>
+          <h2 class="feature-title">Backend Development:
+</h2>
+          <p>Developing the server-side logic, databases, and APIs that power web applications. This can involve programming languages like Python, Ruby, Java, PHP, or Node.js.
+</p>
+        </div>
+        <div class="feature">
+          <div class="feature-icon">&#x1F4A1;</div>
+          <h2 class="feature-title">Database Integration:
+</h2>
+          <p>Setting up, managing, and optimizing databases for storing and retrieving data used by websites and applications.
+</p>
+        </div>
+      </div>
+       <div class="row-1">
+        <div class="feature">
+          <div class="feature-icon">&#x1F4B3;</div>
+          <h2 class="feature-title">Performance Optimization:
+</h2>
+          <p>Enhancing the speed and performance of websites and applications through techniques like caching, minification, and optimizing server-side processes.
+</p>
+        </div>
+        <div class="feature">
+          <div class="feature-icon">&#x1F4B8;</div>
+          <h2 class="feature-title">Web Hosting and Deployment:
+</h2>
+          <p>Assisting with deploying websites to servers, configuring hosting environments, and managing domain names.
+</p>
+        </div>
+        <div class="feature">
+          <div class="feature-icon">&#x1F4A1;</div>
+          <h2 class="feature-title">Consultation and Advice:
+</h2>
+          <p>Providing expertise and guidance on web technologies, best practices, and solutions tailored to the client's business goals.</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</body>
+
+</html>
